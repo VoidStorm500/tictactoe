5e78e18ca441fbd6203ada44ea4fa3b58a1c4fb2
Jake <jlcurrie9001@gmail.com>
2023-07-20 20:47:46 -0500

    3rd commit

--- tictactoe.py
+++ tictactoe.py
@@ -1,2 +1,7 @@
-board = "|1|2|3|\n|4|5|6|\n|7|8|9|"
-#tictactoe game
+import random
+
+class TicTacToe:
+    def create_board():
+        board = "|1|2|3|\n|4|5|6|\n|7|8|9|"
+        print(board)
+    def input_select():
